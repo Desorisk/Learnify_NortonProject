f1aea45969d80f4e086a2b798a26fa91d9e38e4b
Loeun Vandeth <vandeth3412@gmail.com>
2026-06-30 21:58:49 +0700

    .

--- js/profile.js
+++
@@ -1,9 +0,0 @@
-// js/profile.js
-
-if (localStorage.getItem('isLoggedIn') !== 'true') {
-  window.location.href = './login.html';
-}
-
-document.getElementById('profileName').textContent = localStorage.getItem('userName') || 'User';
-document.getElementById('profileEmail').textContent = localStorage.getItem('userEmail') || '';
-document.getElementById('profileAvatar').src = localStorage.getItem('userAvatar') || './public/default-avatar.png';
